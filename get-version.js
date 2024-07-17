477db794f5d9ceae48719a5f0499cb578b829e97
Rangga Fajar Oktariansyah <86386385+FajarKim@users.noreply.github.com>
2024-07-17 18:52:42 +0700

    chore: added comment in the code (#11)

--- get-version.js
+++ get-version.js
@@ -1,5 +1,11 @@
 const axios = require("axios");
 
+/**
+ * Retrieves the version number from a package JSON located at the specified URL.
+ *
+ * @param {string} url - The URL where the package JSON is located.
+ * @returns {Promise<string>} - A promise that resolves to the version string of the package.
+ */
 async function getPackageVersion(url) {
   const response = await axios.get(url);
   const packageJson = response.data;
